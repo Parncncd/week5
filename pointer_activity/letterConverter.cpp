e990077a6cae676a077bb53297c797651cda3bd9
Parncncd <69336494+Parncncd@users.noreply.github.com>
2020-09-12 12:10:00 +0700

    Add project files.

---
+++ pointer_activity/letterConverter.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	char str[50];
+	char* p;
+	p = str;
+	scanf("%s", str);
+	while (*p != '\0') {
+		if ((*p >= 'A') && (*p <= 'Z')) {
+			printf("%c", *p - 'A' + 'a');
+		}
+		else if ((*p >= 'a') && (*p <= 'z')) {
+			printf("%c", *p - 'a' + 'A');
+		}
+		p += 1;
+	}
+	return 0;
+}
